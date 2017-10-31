558ce5bec59e6548898ab988b9989de2f31d69f3
Conrad Zbigniew Lippert-Zajaczkowski <konrad@groupwelldone.com>
2017-10-31 14:48:23 -0500

    removed extra space in function test template

--- function/templates/test/_test.js
+++ function/templates/test/_test.js
@@ -2,9 +2,9 @@ const chai = require('chai');
 const expect = chai.expect;
 const <%= nameCamelCase %> = require('../dist/<%= nameKebabCase%>.js');
 
-describe('<%= nameCamelCase %> works', function () {
-  describe('maybe a bit more context here', function () {
-    it('should run here few assertions', function () {
+describe('<%= nameCamelCase %> works', function() {
+  describe('maybe a bit more context here', function() {
+    it('should run here few assertions', function() {
 
     });
   });
